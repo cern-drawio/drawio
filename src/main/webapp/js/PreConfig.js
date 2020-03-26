539634e9d30fa0ffeea14390cc676381ad3d258a
Esteban Puentes <virtualevan@gmail.com>
2020-03-26 11:42:56 +0100

    Set export server behind the CERNBox proxy

--- src/main/webapp/js/PreConfig.js
+++ src/main/webapp/js/PreConfig.js
@@ -3,6 +3,6 @@
  * Copyright (c) 2006-2020, draw.io AG
  */
 // Overrides of global vars need to be pre-loaded
-window.EXPORT_URL = 'https://drawio-export.web.cern.ch';
+window.EXPORT_URL = 'https://cernbox.cern.ch/byoa/drawioexport';
 window.DRAW_MATH_URL = 'https://cernbox.cern.ch/byoa/drawio/math';
 window.DRAWIO_CONFIG = null; //Replace with your custom draw.io configurations. For more details, https://desk.draw.io/support/solutions/articles/16000058316
