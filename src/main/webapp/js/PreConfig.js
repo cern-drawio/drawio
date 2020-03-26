7d8427e5ba397151cd50c5d7a17cd339bb3ff4ed
Esteban Puentes <virtualevan@gmail.com>
2020-03-26 13:38:00 +0100

    Configure export server

--- src/main/webapp/js/PreConfig.js
+++ src/main/webapp/js/PreConfig.js
@@ -3,7 +3,7 @@
  * Copyright (c) 2006-2019, draw.io AG
  */
 // Overrides of global vars need to be pre-loaded
-window.EXPORT_URL = 'REPLACE_WITH_YOUR_IMAGE_SERVER';
-window.PLANT_URL = 'REPLACE_WITH_YOUR_PLANTUML_SERVER';
-window.DRAW_MATH_URL = 'math';
+window.EXPORT_URL = 'https://cernbox.cern.ch/byoa/drawioexport';
+window.DRAW_MATH_URL = 'https://cernbox.cern.ch/byoa/drawio/math';
+window.PLANT_URL = 'PLANT_IS_NOT_SUPPOERTED';
 window.DRAWIO_CONFIG = null; //Replace with your custom draw.io configurations. For more details, https://desk.draw.io/support/solutions/articles/16000058316
